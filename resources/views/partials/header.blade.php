06a59ad8a28274619da885290e9e4978066f5871
Alethepata <alessia.patane@gmail.com>
2023-11-17 18:06:19 +0100

    [Add] top-bar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,15 +3,30 @@
 @endphp
 
 <header>
-    <div class="menu">
-        <nav>
-            <ul>
-                @foreach ($menu as $item )
-                  <li>
-                    <a href="{{route($item['link'])}}">{{$item['title']}}</a>
-                  </li>
-                @endforeach
-            </ul>
-        </nav>
+    <div class="top-bar">
+        <div class="container d-flex justify-content-between">
+            <nav class="menu">
+                <ul class="d-flex">
+                    @foreach ($menu as $item )
+                      <li>
+                        <a href="{{route($item['link'])}}">{{$item['title']}}</a>
+                      </li>
+                    @endforeach
+                </ul>
+            </nav>
+
+            <div class="logo">
+                <a href="{{route('home')}}">
+                    <img src="/img/boolean-logo.png" alt="">
+                </a>
+            </div>
+
+            <nav class="flex">
+                <a href="#"><i class="fa-regular fa-user"></i></a>
+                <a href="#"><i class="fa-regular fa-heart"></i></a>
+                <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
+            </nav>
+
+        </div>
     </div>
 </header>
