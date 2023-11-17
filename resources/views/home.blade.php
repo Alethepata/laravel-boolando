40fdcda6562178ac0eb9f4f05a2adb5921716f2b
Alethepata <alessia.patane@gmail.com>
2023-11-17 18:58:34 +0100

    [Add] card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,46 @@
+@php
+    $products = config('products');
+@endphp
+
 @extends('layout.main')
 
 @section('content')
-    <div class="container">
-        <h1>prova</h1>
+    <div class="card-container container d-flex flex-wrap justify-content-between">
+
+        @foreach ($products as $product)
+
+           <div class="card">
+                <div class="contenuto-card">
+
+                    <div class="image-card">
+                      <img class="first-img"  src="{{Vite::asset('resources/img/' . $product['frontImage'])}}" alt="item">
+                       <img class="second-img" src="{{Vite::asset('resources/img/'. $product['backImage'])}}" alt="item">
+                    </div>
+
+                    <div class="discount">
+                      <span class="discount-red"></span>
+                      <span class=" discount-green">Sostenibilità</span>
+                    </div>
+
+                    <div class="heart">
+                      <span>&hearts;</span>
+                    </div>
+
+                </div>
+                <div class="text">
+                    <span>{{ $product['brand']}}</span>
+                    <span>{{ $product['name']}}</span>
+                </div>
+
+                <div class="prezzo">
+
+
+
+                </div>
+
+            </div>
+
+        @endforeach
+
     </div>
 @endsection
