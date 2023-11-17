0a482808891225feb38164ee2f0822cfa25d589a
Alethepata <alessia.patane@gmail.com>
2023-11-17 18:06:37 +0100

    [Add] main

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <title>Boolando</title>
 
     @vite('resources/js/app.js')
@@ -11,6 +13,9 @@
 <body>
     @include('partials.header')
 
-    @yield('content')
+    <main>
+        @yield('content')
+    </main>
+
 </body>
 </html>
